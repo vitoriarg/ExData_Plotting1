13ebc778055c957b7dd14b3f24320341311b8b2b
vitoriarg <70482594+vitoriarg@users.noreply.github.com>
2020-10-28 16:58:03 +0100

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,12 +1,14 @@
-
+# please note that the dates in the plots are in portuguese because of the configurations of my program
 library(dplyr)
 library(lubridate)
 
+#reads, edits and subsets data set
 file <- read.table("./household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
 db <- file %>% 
       mutate(Date = strptime(paste(Date, Time), "%d/%m/%Y %H:%M:%S"), Time = NULL) %>%
       subset(Date >= dmy("01/02/2007") & Date < dmy("03/02/2007"))
 
+#creates plot
 png("plot1.png", height = 480, width = 480)
 hist(db$Global_active_power, main = "Global Active Power", col = "orangered1", xlab = "Global Active Power (kilowatts)", freq = TRUE)
 dev.off()
